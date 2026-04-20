c986730d8a378b778a25a379355adddcc6014110
Necomplus <Necomplus@PW0B1KW2>
2026-04-20 11:46:27 -0500

    fix host para render

--- app.py
+++ app.py
@@ -183,4 +183,4 @@ if __name__ == "__main__":
     print("  🌾 Problema de Transporte de Grano")
     print("  Abre en tu navegador: http://localhost:5000")
     print("="*50 + "\n")
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=False)
